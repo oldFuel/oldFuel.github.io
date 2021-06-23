b1ec6c99e13145d5a0683137add3952f24516473
old_fuel <setavr@gmail.com>
2021-06-23 17:43:19 +0300

    second

--- Test tasks/fuse8/src/components/HomeCards.js
+++ Test tasks/fuse8/src/components/HomeCards.js
@@ -8,13 +8,14 @@ function HomeCards({cards}) {
       <div className="wrapper">
 
         <div className="property" key={info.id}>
-            <a className="property-link" href={`/details/${info.id}`}></a>
+            <a className="property-link" href={`/details/${info.id}`}>
           <div className="property-img">
             <div className={`${info.type==='IndependentLiving'?'property-img__type--green':'property-img__type--orange'}`}> {info.type}</div>
           </div>
           <div className="property-title">{info.title}</div>
           <div className="property-address">{info.address}</div>
           <div className="property-price"> New Properties for sale from <b>£{info.price}</b></div>
+            </a>
         </div>
       </div>
     )));
